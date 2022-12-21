df048fde1a23426d30ecab4579fff0f7c8f4a9e4
Derek Rymers <rymersd@gmail.com>
2022-12-21 13:14:46 -0500

    DB prepped no ML data prep

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -35,6 +35,7 @@ CREATE SEQUENCE seq_dose_id
   CACHE 1;
   
 CREATE TABLE users (
+	
 user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
 username varchar(255) NOT NULL UNIQUE,
 password_hash varchar(200) NOT NULL,
@@ -48,8 +49,11 @@ CONSTRAINT PK_user PRIMARY KEY (user_id)
 -- sugar remains on blood cells for three months measure of average blood sugar (user input)
 
 CREATE TABLE user_data (
+	
 user_id int NOT NULL PRIMARY KEY,
 a1c decimal(4, 2),
+target_low int NOT NULL,
+target_high int NOT NULL,
 fasting_glucose int,
 diabetes_type int,
 prediabetic varchar(16),
@@ -70,6 +74,7 @@ CONSTRAINT FK_user_data_user FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE user_measurements (
+	
 user_id int NOT NULL PRIMARY KEY,
 regular_insulin_dose decimal(4,2),
 NPH_insulin_dose decimal(4,2),
@@ -107,6 +112,7 @@ duration int
 );
 
 CREATE TABLE insulin (
+	
 insulin_id int DEFAULT nextval('seq_insulin_id'::regclass) NOT NULL unique,
 base_level decimal(10,3),
 avg_level decimal(10,3),
@@ -120,6 +126,7 @@ CONSTRAINT FK_insulin_insulin_information FOREIGN KEY (insulin_brand_name) REFER
 );
 
 CREATE TABLE insulin_user_data_join (
+	
 user_id int,
 insulin_id int,
 
@@ -129,6 +136,7 @@ CONSTRAINT FK_insulin_user_data_join_user_data FOREIGN KEY (user_id) REFERENCES 
 );
 
 CREATE TABLE meals (
+	
 meal_id int DEFAULT nextval('seq_meal_id'::regclass) NOT NULL unique,
 carbs int NOT NULL,
 food varchar (32),
@@ -137,6 +145,7 @@ date_of_meal DATE DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE meal_information (
+	
 meal_id int NOT NULL PRIMARY KEY,
 glycemic_index decimal(3,2), --FDA API,
 serving_size decimal(3,2),
@@ -175,6 +184,7 @@ CONSTRAINT FK_meal_information_meals FOREIGN KEY (meal_id) REFERENCES meals(meal
 );
 
 CREATE TABLE meals_user_join (
+	
 meal_id int NOT NULL,
 user_id int NOT NULL,
 
@@ -184,15 +194,15 @@ CONSTRAINT FK_meals_user_join_meals FOREIGN KEY (meal_id) REFERENCES meals(meal_
 );
 
 CREATE TABLE blood_sugar (
+	
 blood_sugar_id int DEFAULT nextval('seq_blood_sugar_id'::regclass) NOT NULL unique,
-target_low int NOT NULL,
-target_high int NOT NULL,
 input_level int NOT NULL,
 time_last_measurement TIME DEFAULT CURRENT_TIME,
 date_last_measurement DATE DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE blood_sugar_user_data_join (
+	
 blood_sugar_id int NOT NULL,
 user_id int NOT NULL,
 
@@ -202,6 +212,7 @@ CONSTRAINT FK_blood_sugar_user_data_join_blood_sugar FOREIGN KEY (blood_sugar_id
 );
 
 CREATE TABLE dose (
+	
 blood_sugar_id int NOT NULL PRIMARY KEY,
 input_level int, 
 type_of_dose varchar(32),
@@ -213,5 +224,51 @@ CONSTRAINT FK_dose_blood_sugar FOREIGN KEY (blood_sugar_id) REFERENCES blood_sug
 
 INSERT INTO users (username,password_hash,role) VALUES ('user@example.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role) VALUES ('rymersd@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+
+INSERT INTO user_data (user_id, a1c, target_low, target_high, fasting_glucose, diabetes_type, prediabetic, user_age, weight_kg, height_meters, daily_carb_goal,
+daily_calorie_goal, avg_glycemic_index_goal, emergency_contact_1, emergency_contact_2, date_last_updated, time_last_updated)
+VALUES (1, 5.7, 80, 130, 99, 2, 'No', 37, 75, 1.77, 225, 2000, 55, 'rymersd@gmail.com', 'mdelafay497@gmail.com', '2022/12/21', '12:00');
+
+INSERT INTO user_data (user_id, a1c, target_low, target_high, fasting_glucose, diabetes_type, prediabetic, user_age, weight_kg, height_meters, daily_carb_goal,
+daily_calorie_goal, avg_glycemic_index_goal, emergency_contact_1, emergency_contact_2, date_last_updated, time_last_updated)
+VALUES (3, 5.7, 80, 130, 99, 0, 'Yes', 37, 75, 1.77, 225, 2000, 55, 'rymersd@gmail.com', 'mdelafay497@gmail.com', '2022/12/21', '12:00');
+
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Admelog', 'Rapid-Acting', 51, 15, 15, 60, 180);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Apidra', 'Rapid-Acting', 42, 15, 15, 60, 180);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Fiasp', 'Rapid-Acting', 57, 15, 15, 60, 180);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Humalog', 'Rapid-Acting', 60, 15, 15, 60, 180);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Novolog', 'Rapid-Acting', 81, 15, 15, 60, 180);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Humulin R', 'Short-Acting', 90, 30, 30, 150, 270);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Novolin R', 'Short-Acting', 90, 30, 30, 150, 270);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Humulin N', 'Intermediate-Acting', 264, 180, 180, 1080, 900);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, peak, duration)
+VALUES ('Novolin N', 'Intermediate-Acting', 264, 180, 180, 1080, 900);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, duration)
+VALUES ('Basaglar KwikPen', 'Long-Acting', 720, 180, 180, 1440);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, duration)
+VALUES ('Lantus', 'Long-Acting', 810, 180, 180, 1440);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, duration)
+VALUES ('Levemir', 'Long-Acting', 720, 180, 180, 1440);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, duration)
+VALUES ('Toujeo', 'Long-Acting', 1110, 180, 180, 1440);
+INSERT INTO insulin_information (insulin_brand_name, insulin_type, half_life, onset_low, onset_high, duration)
+VALUES ('TresibaFlexTouch', 'Long-Acting', 810, 180, 180, 1440);
+INSERT INTO insulin (base_level, avg_level, time_last_dose, insulin_brand_name, insulin_strength, insulin_ratio)
+VALUES (10.0, 10.0, '2022/12/12 00:00:00', 'Admelog', 'Strong', 0.14);
+INSERT INTO insulin (base_level, avg_level, time_last_dose, insulin_brand_name, insulin_strength, insulin_ratio)
+VALUES (10.0, 10.0, '2022/12/12 00:00:00', 'Apidra', 'Strong', 0.14);
+INSERT INTO insulin (base_level, avg_level, time_last_dose, insulin_brand_name, insulin_strength, insulin_ratio)
+VALUES (10.0, 10.0, '2022/12/12 00:00:00', 'Fiasp', 'Strong', 0.14);
+INSERT INTO insulin (base_level, avg_level, time_last_dose, insulin_brand_name, insulin_strength, insulin_ratio)
+VALUES (10.0, 10.0, '2022/11/11 00:00:00', 'Fiasp', 'Strong', 0.14);
 
 COMMIT TRANSACTION
