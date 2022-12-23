d797337b5c9a81a7287bca5af8a19d8172a40be2
Derek Rymers <rymersd@gmail.com>
2022-12-23 17:05:23 -0500

    Initial implementation for login

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -11,6 +11,7 @@ import Form from "./scenes/form";
 import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
+import Login from "./scenes/login";
 
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -39,6 +40,7 @@ function App() {
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/login" element={<Login />} />
 
             </Routes>
           </main>
