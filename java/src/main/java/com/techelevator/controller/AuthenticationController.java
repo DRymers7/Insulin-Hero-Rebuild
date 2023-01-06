9279d925ef423e0fb8bb4ae398839ed499bb26a6
Derek Rymers <rymersd@gmail.com>
2023-01-06 13:26:00 -0500

    Updated POM

--- java/src/main/java/com/techelevator/controller/AuthenticationController.java
+++ java/src/main/java/com/techelevator/controller/AuthenticationController.java
@@ -1,7 +1,7 @@
 package com.techelevator.controller;
 
-import javax.validation.Valid;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
